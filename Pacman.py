d6274eda63f90d0d2573be6644d535a1a33ba3a6
Edu980-8 <daniedu151@gmail.com>
2023-06-09 18:13:47 -0500

    Agregando modificaciones al codigo, plantillas de metodos y caracteristicas iniciales

--- Pacman.py
+++ Pacman.py
@@ -17,7 +17,48 @@ class Game:
         self.ROJO = (240,0,0)
         self.VERDE_FONDO = (0,255,0)
         
+        #------Banderas del Juego-------------------#
         self.gameover = True
         self.level_success = True
+        self.pausa_superado_tomartiempo = pygame.time.get_ticks() #Cuando se supere el nivel va a haber una pausa de tiempo
         
-
+        #-------------Tamaño personajes-------------#
+        self.BSX = 50 #Tamaño de cada personaje del mapa fantasma( 50 x 50) y pacman (50 x 50).
+        self.BSY = 50
+        
+        #------------Caracteristicas iniciales------#
+        self.puntos_inicio = 0
+        self.nivel_inicio = 1
+        self.vidas = 3
+        self.PAUSA_MUERTE = 2000
+        self.res_pantalla = (1200,700) # Tamaño de la pantalla de juego
+        self.FPS = 60 #
+        
+        #----------------------Creo la pantalla----------------------------#
+        self.pantalla = pygame.display.set_mode(self.res_pantalla)
+        pygame.display.set_caption("Pacman by EdDesign con machine Learning")
+        self.reloj = pygame.time.Clock()
+        
+        
+        #---------------------Listas de fantasmas y pacman----------------#
+        self.lista_sprites_adibujar = pygame.sprite.Group() #Crea un grupo de sprites vacio para almacenar gestionar y manipular los sprites
+        self.lista_pacman = pygame.sprite.Group()
+        
+        self.crearPantalla()
+        
+    def crearPantalla():
+        pass
+    
+    def new_game(self):
+        self.puntos_inicio = 0
+        self.nivel = 1
+        self.vidas = 3
+        
+    def validar_nivelsuperado(self):
+        pass
+    
+    def pausa_nivelsuperado(self):
+        pass
+    
+    
+    
